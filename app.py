e95795a8ea2b9719d16c74e1e8b84c46a44cef4f
subhechac@gmail.com <subhechac@gmail.com>
2021-04-06 23:41:59 +0530

    Json parsing for journal data and Sentiment Analysis output for journie

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 #import important libraries
+import json
 import os
 import pickle
 from flask import Flask, request, jsonify, render_template
@@ -11,31 +12,52 @@ app = Flask(__name__)
 #load the saved ML model from local
 loaded_model = pickle.load(open('model.pickle', 'rb'))
 
+#checkkey
+def checkKey(dict, key):
+	if key in dict.keys():
+		return True
+	else:
+		return False
+
+#declaring string
+
+
 #runs index.html as homepage for the website automatically
 @app.route("/")
 def home():
-	return render_template("index.html")
+	return render_template("index.html")	
+
+
+
+
+#print(input, flush=True)
+#test= dict([i, True] for i in input.split())
+#print(test, flush=True)
+#output = loaded_model.classify(test)
+#print(type(output), flush=True)
+#return render_template('index.html', show_text='Sentiment : {}'.format(output))
 
 
 @app.route('/classify', methods=['POST'])
 def classify():
-	#for rendering results on html page
-	input = request.form['content']
-	print(input, flush=True)
-	test= dict([i, True] for i in input.split())
-	print(test, flush=True)
-	output = loaded_model.classify(test)
-	return render_template('index.html', show_text='Sentiment : {}'.format(output))
-
-
+    journal_data = request.get_json(force=True)
+    solution= ""
+    for i in range(len(journal_data["blocks"])):
+        if(checkKey(journal_data["blocks"][i]["data"], "style")):
+            i=i+1
+        else:
+            solution=solution+journal_data["blocks"][i]["data"]["text"]
+    sentimentAnalysis = dict([i,True] for i in solution.split())
+    SAmodel = loaded_model.classify(sentimentAnalysis)
+    return jsonify(SAmodel)
 
 @app.route('/classify_api', methods = ['POST'])
 def inputModel():
-	journie_input = request.getjson(force= True)
-	#test= dict([i, True] for i in journie_input.split())
+	journie_input = request.get_json(force= True)
+	test= dict([i, True] for i in journie_input.split())
 	result = loaded_model.classify(journie_input)
-	#print(result)
-	return jsonify(output)
+	print(result)
+	return jsonify(result)
 
 
 #run the app in debug mode
--- request.py
+++ request.py
@@ -1,6 +1,6 @@
-import requests
+import request
 
 url = 'http://127.0.0.1:5000/classify_api'
-r = requests.post(url,json = {'hi': True, 'i': True, 'am': True, 'happy': True})
+r = request.post(url,json = {'hi': True, 'i': True, 'am': True, 'happy': True})
 
 print(r.json)
